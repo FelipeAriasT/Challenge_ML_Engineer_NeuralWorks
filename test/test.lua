4ffd1e8dc7cdca4950e80a4681342f2ad18cd38c
Felipe <felipe.arias.t@ing.uchile.cl>
2023-07-07 01:40:39 -0400

    imagenes y test

---
+++ test/test.lua
@@ -0,0 +1,47 @@
+wrk.method = "POST"
+wrk.headers["Content-Type"] = "application/json"
+wrk.body = [[
+    {
+    "vuelos": [
+        {
+            "OPERA_Aerolineas_Argentinas": true,
+            "OPERA_Aeromexico": false,
+            "OPERA_Air_Canada": true,
+            "OPERA_Air_France": false,
+            "OPERA_Alitalia": true,
+            "OPERA_American_Airlines": false,
+            "OPERA_Austral": true,
+            "OPERA_Avianca": false,
+            "OPERA_British_Airways": true,
+            "OPERA_Copa_Air": false,
+            "OPERA_Delta_Air": true,
+            "OPERA_Gol_Trans": false,
+            "OPERA_Grupo_LATAM": true,
+            "OPERA_Iberia": false,
+            "OPERA_JetSmart_SPA": true,
+            "OPERA_K_L_M": false,
+            "OPERA_Lacsa": true,
+            "OPERA_Latin_American_Wings": false,
+            "OPERA_Oceanair_Linhas_Aereas": true,
+            "OPERA_Plus_Ultra_Lineas_Aereas": false,
+            "OPERA_Qantas_Airways": true,
+            "OPERA_Sky_Airline": false,
+            "OPERA_United_Airlines": true,
+            "TIPOVUELO_I": false,
+            "TIPOVUELO_N": true,
+            "MES_1": false,
+            "MES_2": true,
+            "MES_3": false,
+            "MES_4": true,
+            "MES_5": false,
+            "MES_6": true,
+            "MES_7": false,
+            "MES_8": true,
+            "MES_9": false,
+            "MES_10": true,
+            "MES_11": false,
+            "MES_12": true
+        }
+    ]
+}
+]]
